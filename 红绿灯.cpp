5c98389c31346d7408a39ac3abf3d08d8692c8a9
moYu <jzf221419168@163.com>
2023-03-20 12:12:25 +0800

    update

--- 红绿灯.cpp
+++ 红绿灯.cpp
@@ -7,37 +7,35 @@ const int INF = 0x3f3f3f;
 int main()
 {
   // 请在此输入您的代码
-  int N, M, K, V;
-  cin >> N >> M >> K >> V;
+  int n, m, k, v;
+  cin >> n >> m >> k >> v;
   // 每个结点所需时间
-  vector<int> times(M + 2, 0);
+  vector<int> times(m + 2, 0);
   // 0为绿灯持续时间， 1为总时间
-  int t[M + 2][2];
-  for(int i = 1; i <= M ;i++){
+  int t[m + 2][2];
+  for(int i = 1; i <= m ;i++){
     cin >> times[i];
-    times[i] -= times[i - 1];
     cin >> t[i][0];
     cin >> t[i][1];
-    t[i][1] += t[i][0];
   }
   // 最后一个红绿灯到公司的举例，假设为绿灯时间占比为 100%
-  t[M + 1][0] = t[M + 1][1] = 0;
+  t[m + 1][0] = t[m + 1][1] = 1;
 
-  times[M + 1] = (N - times[M]) * V;
+  times[m + 1] = n;
 
-  for(int i = 1; i <= M + 1; i++)
-    times[i] *= V;
+  for(int i = m + 1; i > 0; i--)
+    times[i] =(times[i] - times[i-1]) * v;
   
-  vector<int> dp (K, 0);
-  for(int i = 1; i <= M + 1; i++){
-    int tmp = dp[K - 1];
-    for(int j = K - 1; j > 0; j--){
+  vector<int> dp (k, 0);
+  for(int i = 1; i <= m + 1; i++){
+    int tmp = dp[k - 1];
+    for(int j = k - 1; j > 0; j--){
       dp[j] = min(dp[j], dp[j-1] + times[i] + ( (dp[j-1] + times[i]) % t[i][1] <= t[i][0] ? 0 : t[i][1] - (dp[j-1] - times[i]) % t[i][1] ) );
     }
-    dp[0] = min(dp[0], dp[K - 1]);
+    dp[0] = min(dp[0], dp[k - 1]);
   }
   int ans = INF;
-  for(int i = 0; i < K; i++) ans = min(ans, dp[i]);
+  for(int i = 0; i < k; i++) ans = min(ans, dp[i]);
   cout << ans << endl;
   return 0;
 }
